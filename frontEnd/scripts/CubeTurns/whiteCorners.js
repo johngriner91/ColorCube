bb5cc59f734fba9dbd28f3486bbe03e70d05b59f
John Griner <johngriner91@gmail.com>
2015-06-03 17:43:14 -0400

    front end

---
+++ frontEnd/scripts/CubeTurns/whiteCorners.js
@@ -0,0 +1,8 @@
+// ---------------------------------------------------------------
+//  This is the function that receives an array of colors and
+//    solves the cube, returning a set of turns.
+// ---------------------------------------------------------------
+
+function whiteCorners() {
+    alert("We are in the white Corners function");
+}
---
+++ frontEnd/scripts/solve_cube.js
@@ -0,0 +1,9 @@
+// ---------------------------------------------------------------
+//  This is the function that receives an array of colors and
+//    solves the cube, returning a set of turns.
+// ---------------------------------------------------------------
+
+function solve() {
+    alert("We are in the solve function");
+    whiteCorners();
+}
